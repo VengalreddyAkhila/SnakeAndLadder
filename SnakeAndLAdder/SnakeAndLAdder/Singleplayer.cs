6a3cd3cc97f0892ae534b661e826696ceb48a2b5
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-28 22:13:24 +0530

    Refactor:SinglePlayer

---
+++ SnakeAndLAdder/SnakeAndLAdder/Singleplayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLAdder
+{
+     public class Singleplayer
+    {
+        public void player()
+        {
+            int position = 0;
+            Console.WriteLine("Start the game");
+            Console.WriteLine("position :" + position);
+        }
+    }
+}
