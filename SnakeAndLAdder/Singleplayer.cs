6b9a75c2147bff6a7543d90ed29da55048f803ee
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-30 10:03:42 +0530

    Refactor:Uc2-Dice

--- SnakeAndLAdder/Singleplayer.cs
+++ SnakeAndLAdder/Singleplayer.cs
@@ -11,6 +11,9 @@ namespace SnakeAndLAdder
             int position = 0;
             Console.WriteLine("Start the game");
             Console.WriteLine("position :" + position);
+            Random random = new Random();
+            int dice = random.Next(1, 6);
+            Console.WriteLine("Your Dice number is:" + dice);
         }
     }
 }
