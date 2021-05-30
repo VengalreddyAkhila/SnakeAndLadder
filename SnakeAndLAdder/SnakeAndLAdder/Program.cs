e364127c99e3c30a54f9e8d1426a825390887cc5
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-30 17:17:56 +0530

    Refactor:UC3-SnakeAndLadder

---
+++ SnakeAndLAdder/SnakeAndLAdder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder");
+            Singleplayer singleplayer = new Singleplayer();
+            singleplayer.player();
+        }
+    }
+}
---
+++ SnakeAndLAdder/SnakeAndLAdder/Singleplayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLAdder
+{
+     public class Singleplayer
+    {
+        public void player()
+        {
+            int position = 0;
+            int newposition = 0;
+            Console.WriteLine("Start the game");
+            Console.WriteLine("position :" + position);
+            Random random = new Random();
+            int dice = random.Next(1, 6);
+            Console.WriteLine("Dice number is:" + dice);
+
+
+            Console.WriteLine("Enter the choice:");
+            String answer = Convert.ToString(Console.ReadLine());
+            //if (answer == "y")
+            //Choice to check its snake or ladder
+            int Choice = random.Next(1, 2);
+            switch (Choice)
+            {
+                case 1:
+                    //ladder
+                    position = dice;
+                    Console.WriteLine("current position is : " + position);
+                    break;
+
+                case 2:
+                    //snake
+                    //if (position > dice)
+
+                    //{
+                    position = dice;
+                    Console.WriteLine("current position is : " + position);
+                    //}
+                    break;
+
+                case 3:
+                    //no play
+                    position = 0;
+                    Console.WriteLine("current position is : " + position);
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+            }
+            position = position + newposition;
+
+            if (position < 0)
+            {
+                position = 0;
+            }
+            Console.WriteLine("You are on Position " + position);
+        }
+
+            //Console.WriteLine("dice" + dice);
+            //}
+
+
+        
+    }
+} 
