2eb35514a885a7afdeb95614e1fdffff6df80196
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-01 21:49:07 +0530

    Refactor:UC5-ExactWinning

--- SnakeAndLAdder/Singleplayer.cs
+++ SnakeAndLAdder/Singleplayer.cs
@@ -11,17 +11,23 @@ namespace SnakeAndLAdder
             //single player position is 0
             int position = 0;
             Console.WriteLine("position :" + position);
-            //player rolls the die
-            Random random = new Random();
-            int dice = random.Next(1, 6);
-            Console.WriteLine("Dice number is:" + dice);
-            int Choice = random.Next(1, 2);
-            //player checks options noplay,ladder,snake
+            //player checks winning position 100
+            while (position < 100)
+            {
+                //player rolls the die
+                Random random = new Random();
+                int dice = random.Next(1, 6);
+                Console.WriteLine("Dice number is:" + dice);
+                int Choice = random.Next(1, 2);
+                //player checks options noplay,ladder,snake
                 switch (Choice)
                 {
                     case 1:
-                        position = position + dice;
-                        Console.WriteLine("Its a ladder" + position);
+                        if (position > 100)
+                            Console.WriteLine("you win");
+                        else
+                            position = position + dice;
+                            Console.WriteLine("Its a ladder" + position);
                         break;
                     case 2:
                         if (position < 0)
@@ -40,6 +46,7 @@ namespace SnakeAndLAdder
                         break;
                 }
                 Console.WriteLine("your position is : " + position);
+            }
             
         }
     }
