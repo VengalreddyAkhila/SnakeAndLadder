09b4a729ca7b9110b8ee58f4566c5fc167356046
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-01 21:35:15 +0530

    Refactor:UC4-WinningPosition100

--- SnakeAndLAdder/Singleplayer.cs
+++ SnakeAndLAdder/Singleplayer.cs
@@ -11,35 +11,39 @@ namespace SnakeAndLAdder
             //single player position is 0
             int position = 0;
             Console.WriteLine("position :" + position);
-            //player rolls the die
-            Random random = new Random();
+            //checks winning position 100
+            while (position < 100)
+            {
+                //player rolls the die
+                Random random = new Random();
             int dice = random.Next(1, 6);
             Console.WriteLine("Dice number is:" + dice);
             int Choice = random.Next(1, 2);
             //player checks options noplay,ladder,snake
-            switch (Choice)
-            {
-                case 1:
-                    position = position + dice;
-                    Console.WriteLine("Its a ladder" + position);
-                    break;
-                case 2:
-                    if (position < 0)
-                    Console.WriteLine("you lose");
-                    else
-                    position = position - dice;
-                    Console.WriteLine("Its a Snake" + position);
-                    break;
+                switch (Choice)
+                {
+                    case 1:
+                        position = position + dice;
+                        Console.WriteLine("Its a ladder" + position);
+                        break;
+                    case 2:
+                        if (position < 0)
+                            Console.WriteLine("you lose");
+                        else
+                            position = position - dice;
+                        Console.WriteLine("Its a Snake" + position);
+                        break;
 
-                case 3:
-                    position = 0;
-                    Console.WriteLine("current position is : " + position);
-                    break;
-                default:
-                    Console.WriteLine("invalid choice");
-                    break;
+                    case 3:
+                        position = 0;
+                        Console.WriteLine("current position is : " + position);
+                        break;
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+                }
+                Console.WriteLine("your position is : " + position);
             }
-            Console.WriteLine("your position is : " + position);
         }
     }
 }
